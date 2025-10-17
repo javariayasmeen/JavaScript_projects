c128727a8dd353ec5bd4bb893f34ca2383fe13a2
javariayasmeen <javariayasmeen681@gmail.com>
2025-10-17 17:08:40 +0500

    done

---
+++ Class.js
@@ -0,0 +1,6 @@
+const paragraphColor = () =>{
+    let paras = document.querySelectorAll(".myPara");
+    paras[0].style.color = "red";
+    paras[1].style.color = "green";
+    paras[2].style.color = "skyblue";
+}
---
+++ Images.js
@@ -0,0 +1,7 @@
+const borderStyles = () => {
+    let images = document.querySelectorAll(".myImage");
+    images[0].style.border = "4px solid blue";
+    images[1].style.border = "4px dotted green";
+    images[2].style.border = "4px dashed purple";
+    images[3].style.border = "4px double black";
+}
