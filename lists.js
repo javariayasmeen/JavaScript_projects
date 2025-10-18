a9efbb975bc7e08b661133b2c8da2dee6098cecf
javariayasmeen <javariayasmeen681@gmail.com>
2025-10-18 12:53:20 +0500

    done

---
+++ lists.js
@@ -0,0 +1,24 @@
+let countries = [
+    "Turkey",
+    "Finland",
+    "Germany",
+    "Candaa",
+    "Netherland",
+    "Romania",
+    "Australia",
+    "Nepal",
+    "Scotland",
+    "Lithuana",
+];
+let one = document.getElementById("one");
+let two = document.getElementById("two");
+let unOrderedList="";
+let orderedList = "";
+for (let i=0; i < countries.length; i++) {
+   unOrderedList = unOrderedList + "<li>" + countries[i] + "</li>";
+   orderedList = orderedList + "<li>" + countries[i] + "</li>";
+   console.log(unOrderedList); 
+   console.log(orderedList);
+} 
+one.innerHTML = unOrderedList;
+two.innerHTML = orderedList;
